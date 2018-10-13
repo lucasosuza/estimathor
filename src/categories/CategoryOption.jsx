e8128bc30104da0dfbe9b39cbad0cd7a611de545
Lucas Souza <lucas.b.souza@gmail.com>
2018-10-12 22:46:57 -0300

    Added selecion action to CategoryOption

--- src/categories/CategoryOption.jsx
+++ src/categories/CategoryOption.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 
 class CategoryOption extends Component {
-  state = {};
+  state = {
+    selected: false
+  };
+
+  handleSelect = () => {
+    const selected = !this.state.selected;
+    this.setState({ selected });
+  };
+
+  getSelectedClass() {
+    let classes = "card ";
+    classes += this.state.selected ? "text-white bg-dark" : "";
+
+    return classes;
+  }
+
   render() {
     const { title, description } = this.props.option;
     return (
       <div className="col-sm-4">
-        <div className="card">
+        <div className={this.getSelectedClass()}>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <button className="btn btn-primary">Select</button>
+            <button className="btn btn-primary" onClick={this.handleSelect}>
+              Select
+            </button>
           </div>
         </div>
       </div>
